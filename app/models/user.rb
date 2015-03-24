11ffb8709a2e9bfaf7332b83ee99b4b917bc70fc
aschmid1 <aschmid1@lakeheadu.ca>
2015-03-24 19:27:52 -0400

    User model email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,5 +10,12 @@
 #
 
 class User < ActiveRecord::Base
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :email, presence: true,
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
   has_secure_password
+
+  before_save { self.email.downcase! }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -12,7 +12,34 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "emails should be unique (case-insensitive)" do
+    existing = users(:one)
+
+    user = User.new(email: existing.email.upcase, password_digest: 'secret')
+    assert user.invalid?
+  end
+
+  test "emails should save as lower case" do
+    user = User.new(email: 'NEW@EMAIL.COM', password_digest: 'secret')
+    user.save
+    assert_equal 'new@email.com', user.email
+  end
+
+  test "should accept valid emails" do
+    valid_emails = %w[foo@bar.com user.foo@bar.com foo+extra@bar.ca]
+    valid_emails.each do |email|
+      user = User.new(email: email, password_digest: 'secret')
+      assert user.valid?
+    end
+  end
+
+  test "should reject invalid emails" do
+    invalid_emails = %w[foo@bar. foo@bar,com foo@bar.c@m]
+    invalid_emails.each do |email|
+      user = User.new(email: email, password_digest: 'secret')
+      assert user.invalid?
+    end
+  end
+
 end
