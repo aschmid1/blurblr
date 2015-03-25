cda8938418fc3fd890736b45cfd7ae29dd4e9179
aschmid1 <aschmid1@lakeheadu.ca>
2015-03-24 20:08:50 -0400

    Move creating new_user to user_test#setup and test that password is not empty.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -12,34 +12,40 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  setup do
+    @user = users(:one)
+    @new_user = User.new(email: 'NEW@email.com', password_digest: @user.password_digest)
+  end
 
   test "emails should be unique (case-insensitive)" do
-    existing = users(:one)
-
-    user = User.new(email: existing.email.upcase, password_digest: 'secret')
-    assert user.invalid?
+    @new_user.email = @user.email.upcase
+    assert @new_user.invalid?
   end
 
   test "emails should save as lower case" do
-    user = User.new(email: 'NEW@EMAIL.COM', password_digest: 'secret')
-    user.save
-    assert_equal 'new@email.com', user.email
+    lower_case_email = @new_user.email.downcase
+    @new_user.save
+    assert_equal lower_case_email, @new_user.email
   end
 
   test "should accept valid emails" do
     valid_emails = %w[foo@bar.com user.foo@bar.com foo+extra@bar.ca]
     valid_emails.each do |email|
-      user = User.new(email: email, password_digest: 'secret')
-      assert user.valid?
+      @new_user.email = email
+      assert @new_user.valid?
     end
   end
 
   test "should reject invalid emails" do
     invalid_emails = %w[foo@bar. foo@bar,com foo@bar.c@m]
     invalid_emails.each do |email|
-      user = User.new(email: email, password_digest: 'secret')
-      assert user.invalid?
+      @new_user.email = email
+      assert @new_user.invalid?
     end
   end
 
+  test "should require password" do
+    @new_user.password_digest = ''
+    assert @new_user.invalid?
+  end
 end
