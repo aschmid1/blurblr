0fde6eedc056a9da5a224c95952ec181207bf4fe
aschmid1 <aschmid1@lakeheadu.ca>
2015-03-28 00:06:53 -0400

    Home page mapped to root.

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
     get :about
   end
 
+  root 'static_pages#home'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
